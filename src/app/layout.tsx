dc221792e38a2fc4c599551a477b7f24db89a295
xividi <xividi123@gmail.com>
2024-10-06 21:03:05 +0200

    added working themes and change button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "~/styles/globals.css";
 import {GeistSans} from "geist/font/sans";
 import {type Metadata} from "next";
 import TanstackProvider from "~/components/tanstack-provider";
+import {ThemeProvider} from "~/components/theme-provider";
+import {ModeToggle} from "~/components/theme-toggle";
 
 export const metadata: Metadata = {
     title: "Salary Tracker",
@@ -10,15 +12,32 @@ export const metadata: Metadata = {
     icons: [{rel: "icon", url: "/favicon.ico"}],
 };
 
+function TopNav() {
+    return (
+        <nav className="flex w-full justify-between border-b p-4 text-xl font-semibold text-center">
+            <div><span className="text-[hsl(280,100%,70%)]">Salary</span>Tracker</div>
+            <ModeToggle/>
+        </nav>
+    );
+}
+
 export default function RootLayout({
                                        children,
                                    }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en" className={`${GeistSans.variable}`}>
-        <body>
-        <TanstackProvider>
-            {children}
-        </TanstackProvider>
+        <body className="flex flex-col gap-4">
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <TanstackProvider>
+                <TopNav/>
+                {children}
+            </TanstackProvider>
+        </ThemeProvider>
         </body>
         </html>
     );
